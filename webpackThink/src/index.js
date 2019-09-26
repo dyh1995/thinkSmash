2d0b4f12d24c41ad29a4a488500d9cff436a6826
dongyuhao <dongyuhao1995@qq.com>
2019-09-26 09:45:49 +0800

    modify

--- webpackThink/src/index.js
+++ webpackThink/src/index.js
@@ -1,4 +1,4 @@
 import fnaname from './fna.js';
 // var fnaname = require('./fna.js');
 // var fnaname = import('./fna.js');
-console.log('Hello World!'+fnaname.name)
+console.log('Hello World!'+fnaname.name);
