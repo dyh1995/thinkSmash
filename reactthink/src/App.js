b6cfe403ded0b40b5ddd25fc0932a2154d3b59e7
dongyuhao <dongyuhao1995@qq.com>
2019-10-08 19:14:39 +0800

    react

--- reactthink/src/App.js
+++ reactthink/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
 import IndexJsx from './jsx/index.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="app-header">
         <IndexJsx/>
       </header>
     </div>
--- reactthink/src/jsx/index.jsx
+++ reactthink/src/jsx/index.jsx
@@ -10,7 +10,7 @@ export default function Index(props){
     return (
         <div>
             <p>Edit <code>src/App.js</code> and save to reload.</p>
-            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+            <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
         </div>
     )
-} 
+}
