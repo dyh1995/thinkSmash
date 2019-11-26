c927c0b4dc9079c162dcf011a0a1b5f559565b0c
dongyuhao <dongyuhao1995@qq.com>
2019-11-26 23:56:41 +0800

    amd,cmd,commonjs

--- daily/一些js的封装方法和日常小技巧.js
+++ daily/一些js的封装方法和日常小技巧.js
@@ -406,3 +406,31 @@ node.cloneNode(deep);
  * appendChild的坑
  */
 node.appendChild(node.childNodes[0]); //当node已经存在子节点childNodes[0]时，复制先删除当前子节点，再追加该childNodes[0]
+
+/**
+ * cmd amd commonjs
+ */
+/**
+ * 1.AMD-异步模块定义,依赖前置；
+ * AMD是RequireJS在推广过程中对模块定义的规范化产出
+ * define(['package/lib'], function(lib){
+ *      return {}
+ * });
+ * 
+ * 2）CMD---是SeaJS在推广过程中对模块定义的规范化产出，是一个同步模块定义，就近依赖
+ * 是SeaJS的一个标准，SeaJS是CMD概念的一个实现，SeaJS是淘宝团队提供的一个模块开发的js框架.
+ * define(function(require, exports, module){
+ *      var $ = require('jquery');
+ * });
+ * 
+ * 3）CommonJS规范---是通过module.exports定义的，在前端浏览器里面并不支持module.exports,
+ * 通过node.js后端使用的。Nodejs端是使用CommonJS规范的，前端浏览器一般使用AMD、CMD、ES6等定义模块化开发的
+ * exports.aaa = function(){}
+ * module.exports = function(){}
+ * 
+ * var aaa = require('./aaa.js')
+ * 
+ * 4）ES6特性，模块化---export/import对模块进行导出导入的
+ * export default {}
+ * import aa from '';
+ *  */
